99787b4b282e10cd7e583d73cb18c5b895f14f07
nattyseydi <nattyseydi@gmail.com>
2012-04-30 16:31:32 +0000

    setting up new repository of nattyseydi v2

---
+++ db/v2natty001-30-04-12.sql
@@ -0,0 +1,4 @@
+A Drupal installation directory could not be found                                                         [31;40m[1m[error][0m
+Command sql-dump needs a higher bootstrap level to run - you will need invoke drush from a more functional [31;40m[1m[error][0m
+Drupal environment to run this command.
+The drush command 'sql-dump' could not be executed.                                                        [31;40m[1m[error][0m
